41cfda079b9464e8982c21cac7ba3af7582ecf28
The Lost One <filippov.ia@phystech.edu>
2025-05-13 20:06:46 +0300

    Ну, по крайней мере оно собирается

--- include/core/prefs.h
+++ include/core/prefs.h
@@ -1,7 +1,7 @@
 #pragma once
 /* User configuration */
 
-#define IWDG_ENABLE
+//#define IWDG_ENABLE
 #define STARTUP_BLINK_ENABLE
 #define SW_LOGGING_ENABLE
 
--- lib/lwip/port/lwipopts.h
+++ lib/lwip/port/lwipopts.h
@@ -2,7 +2,7 @@
 #define LWIOPTS_H_
 
 #define NO_SYS          1
-#define IP_FORWARD      0
+
 #define LWIP_RAW        1
 #define LWIP_DHCP       0
 #define LWIP_SNMP       0
@@ -10,6 +10,11 @@
 #define LWIP_DNS        0
 #define LWIP_TCP        1
 #define LWIP_IP         1
+#define LWIP_ARP        1
+#define LWIP_ICMP       1
+#define LWIP_PPP_API    0
+
+#define IP_FORWARD      0
 
 #define LWIP_SOCKET     0
 #define LWIP_NETCONN    0
