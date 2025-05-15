e53d6621582fa9a1c1adad17cf5a6d6771b71773
The Lost One <filippov.ia@phystech.edu>
2025-05-15 19:16:52 +0300

    Предварительный драйвер вроде готов

--- include/app/net.h
+++ include/app/net.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "uip.h"
 #include "clock-arch.h"
+#include "timer.h"
+#include "uip_arp.h"
 #include "utils/service.h"
 
 enum Net_err {
@@ -11,4 +13,3 @@ enum Net_err {
 typedef enum Net_err Net_err_t;
 
 int net_init(void);
-int app_poll(void);
